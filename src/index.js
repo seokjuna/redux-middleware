c64e95f07fc28b3c77288d1f37e7f3795f907a07
naseokju <lukas95@naver.com>
2022-12-19 14:35:32 +0900

    redux-thunk

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
-import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -4,12 +4,24 @@ import { createAction, handleActions } from "redux-actions";
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-// 액션 생성 함수 만들기 (createAction 사용)
+// 액션 생성 함수 만들기 
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+// Thunk 생성 함수 만들기
+// 1초 뒤에 increase 혹은 decrease 함수를 디스패치
+export const increaseAsync = () => dispatch => {
+    setTimeout(() => {
+        dispatch(increase());
+    }, 1000)
+};
+export const decreaseAsync = () => dispatch => {
+    setTimeout(() => {
+        dispatch(decrease());
+    }, 1000)
+};
 
-// 초기 상태 및  리듀서 함수 만들기 (handleAction 사
+// 초기 상태 및  리듀서 함수 만들기 
 const initialState = 0; // 상태는 꼭 객체일 필요는 없음
 
 const counter = handleActions(
